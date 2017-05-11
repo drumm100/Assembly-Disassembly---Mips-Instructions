e1253835d02fe0403bb550fc4870ad6f6ab16d46
andriuslima <andrius.lima@neogrid.com>
2017-05-11 17:33:53 -0300

    Funcao docde_r parcialmente pronto, falta o caso com shift

--- Disassembly.py
+++ Disassembly.py
@@ -3,35 +3,43 @@
 fileIn = open("teste.asm", "r")
 fileOut = open("teste.txt", "w")
 
-functions = {'sub':34}
-type_r = { 'addu':0, 'addiu':0 , 'subu':0, 'sub':0, 'or':0, 'slt':0, 'sll':0, 'srl':0 }
-type_i = ["xori", "lui", "sltiu", "andi", "beq", "bne"]
-type_j = ["j"]
-type_i_special = ["lw", "sw"]
-register = ["$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0",
+functions = {'sub':34, 'or':37} # Codigo de cada funcao na ULA
+type_r = { 'addu':0, 'addiu':0 , 'subu':0, 'sub':0, 'or':0, 'slt':0, 'sll':0, 'srl':0 } # Instrucoes do tipo R
+type_i = ["xori", "lui", "sltiu", "andi", "beq", "bne"] # Instrucoes do tipo I
+type_j = ["j"] #Instrucoes do tipo J
+type_i_special = ["lw", "sw"] # Instrucoes especiais
+
+register = ["$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", # registradores do MIPS
             "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1",
             "$s2", "$s3", "$s4", "$s5","$s6", "$s7", "$t8", "$t9", "$k0",
             "$k1", "$gp", "$sp", "$fp", "$ra"]
 
 
 
-def decode_r(ins, rd, rs, rt): # precisa colocar com os tamanhos de bits certinhos, ins = 6 bits; rs,rt,rd,shamt = 6 bits e etc
-    shamt = 0
-    instruction = int( bin(type_r[ins]).replace('0b','') )
-    print(instruction)
-    reg1 = int( bin(register.index(rs)).replace('0b','') )
-    print(reg1)
-    reg2 = int( bin(register.index(rt)).replace('0b','') )
-    print(reg2)
-    reg3 = int( bin(register.index(rd)).replace('0b','') )
-    print(reg3)
-    fun = int( hex(functions[ins]).replace('0x','') )
-    print(fun)
-    out = hex(instruction + reg1 + reg2 + reg3 + shamt + fun)
-    print(out)
+def decode_r(ins, rd, rs, rt):
+    shamt = '0'
+
+    instruction = bin(type_r[ins])[2:].zfill(6)
+    reg1 = bin(register.index(rs))[2:].zfill(5)
+    reg2 = bin(register.index(rt))[2:].zfill(5)
+    reg3 = bin(register.index(rd))[2:].zfill(5)
+    shamt = bin( int(shamt.zfill(6)) )[2:].zfill(5)
+    fun = bin(functions[ins])[2:].zfill(6)
+
+    outBin = instruction + reg1 + reg2 + reg3 + shamt + fun
+    output = "0x"
+
+    for i in range(0, len(outBin) - 3, 4 ):
+        aux = outBin[i : i+4]
+        aux = int(aux, 2)
+        aux = hex(aux)[2:]
+        output = output + aux
+
+    fileOut.write(output+'\n')
+
 
 def decode_i(rt, rs, im):
-    print("Compara " + rt + " com " + rs + " e salta ou não para " + im)
+    print("FAZER")
 
 
 for line in fileIn:
--- teste.asm
+++ teste.asm
@@ -3,3 +3,4 @@
 
  main:
        sub $t1, $s3, $s4
+       or $t0, $t1, $t2
