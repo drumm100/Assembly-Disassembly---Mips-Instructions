794229036feb3c4e9db6f4aa8bc1cf391c0c5af2
Marcelo <marcelo.drumm@acad.pucrs.br>
2017-05-12 11:21:51 -0300

    criação Assembly

--- Assembly.py
+++ Assembly.py
@@ -1,4 +1,25 @@
 # Arquivo que gera o código em assembly apartir da decodificação em hexadecimal
 
 fileIn = open("teste.txt", "r")
+# coisa.fill
+
+
+
+aux = hex(fileIn)
+vetor = aux.split('x')                 
+vetor = vetor[0] #vetor agora possui binario
+
+
 fileOut = open("teste.asm", "w")
+
+
+
+
+def converteBinario():
+    #converte binarios com numero de bits adequados
+
+def separaBinario():
+    #separa o todo para cada instrução
+
+def binarioParaInstrucao():
+    #le o binario e retorna a instrução    
