ac6e5ac88bde781e08016f4707f505b6b1b9d472
Andriuslima <andriuslima@outlook.com.br>
2017-05-15 00:14:19 -0300

    Decodificação bne/beq parcialmente

--- Disassembly.py
+++ Disassembly.py
@@ -17,20 +17,18 @@ register = ["$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0",
             "$s2", "$s3", "$s4", "$s5","$s6", "$s7", "$t8", "$t9", "$k0",
             "$k1", "$gp", "$sp", "$fp", "$ra"]
 
-# def find_label(label, line):
-#     # text = fileIn.readlines()
-#     #
-#     # for i in range(0 - line, len(text) ):
-#     #     if label in text[i]: return i
-#
-#     it = line*(-1)
-#     while 1:
-#         lines = fileIn.readlines(it)
-#         if lines != '\n':
-#             if label in lines: return it
-#             it += 1
 
 
+def find_label(label, currentLine):
+    none_ins = 0
+    for i in file:
+        if i != '\n':
+            instruction = i.split()
+            if len(instruction) == 1: none_ins += 1
+
+            if label in instruction[0]:
+                none_ins -= 1
+                return file.index(i) - currentLine - 1 - none_ins
 
 
 def decode_r(ins, rd, rs, rt):
@@ -70,8 +68,7 @@ def decode_r(ins, rd, rs, rt):
     fileOut.write(output+'\n')
 
 
-def decode_i(ins, r1, r2, im, line):
-    return 0
+# def decode_i(ins, r1, r2, im, line):
     # addiu r1, r2, im | opcode(6) ; r2 ; r1 ; imm(16)
     # xori r1, r2, imm | opcode(6) ; r2 ; r1 ; imm(16)
     # lui r1, r2       | opcode(6) ; 0 ; r1 ; r2(16)       --- caso especial
@@ -81,53 +78,55 @@ def decode_i(ins, r1, r2, im, line):
     # sw r1, imm       | opcode(6) ; r1 ; r2 ; imm(16)    --- caso especial
 
 
-# def decode_branch(ins, rs, rt, label, line):
-#     #return 0
-#     instruction = bin(type_i_branch[ins])[2:].zfill(6)
-#     out_rs = bin(register.index(rs))[2:].zfill(5)
-#     out_rt = bin(register.index(rt))[2:].zfill(5)
-#
-#     findLine = find_label(label+":", line)
-#
-#     offset = bin( 4 * (findLine - (line + 1) ) )[2:].zfill(16)
-#
-#     outBin = instruction + out_rs + out_rt + offset
-#     #print(outBin)
-#     output = "0x"
-#
-#     for i in range(0, len(outBin) - 3, 4 ):
-#         aux = outBin[i : i+4]
-#         #print(aux)
-#         aux = int(aux, 2)
-#         aux = hex(aux)[2:]
-#         #print(aux)
-#         output = output + aux
-#         #print(aux)
-#
-#     fileOut.write(output+'\n')
+def decode_branch(ins, rs, rt, label, line):
+    print(line)
+    print(find_label(label, line))
+
+    instruction = bin(type_i_branch[ins])[2:].zfill(6)
+    out_rs = bin(register.index(rs))[2:].zfill(5)
+    out_rt = bin(register.index(rt))[2:].zfill(5)
+
+    findLine = find_label(label, line)
+
+    offset = bin(findLine)[2:].zfill(16)
+
+    outBin = instruction + out_rs + out_rt + offset
+    #print(outBin)
+    output = "0x"
+
+    for i in range(0, len(outBin) - 3, 4 ):
+        aux = outBin[i : i+4]
+        #print(aux)
+        aux = int(aux, 2)
+        aux = hex(aux)[2:]
+        #print(aux)
+        output = output + aux
+        #print(aux)
+
+    fileOut.write(output+'\n')
 
 
+file = fileIn.readlines()
+for item in range(0, len(file) - 1):
+    if item >= len(file): break
+    if ".text" in file[item]: file.pop(item)
+    if ".globl" in file[item]:file.pop(item)
+    if "main:" in file[item]: file.pop(item)
 
-lineNumber = 1
-for line in fileIn:
-    print("leitura linha " + str(lineNumber) )
+for xline in file:
+    if xline == '\n': file.remove(xline)
+
+for line in file:
     if line != '\n':
         instruction = line.split()
 
-        for i in range(0, len(instruction)):  # elimina as virgulas dos elementos do array
+        for i in range(0, len(instruction)):
             instruction[i] = instruction[i].strip(",")
 
         if instruction[0] in type_r:
             decode_r(instruction[0], instruction[1], instruction[2], instruction[3])
-
-        if instruction[0] in type_i:
-            decode_i(instruction[0], instruction[1], instruction[2], instruction[3], lineNumber)
-
-        #if instruction[0] in type_i_branch:
-            #decode_branch( instruction[0], instruction[1], instruction[2], instruction[3], lineNumber )
-
-        lineNumber += 1
-
+        if instruction[0] in type_i_branch:
+            decode_branch(instruction[0], instruction[1], instruction[2], instruction[3], file.index(line))
 
 
 fileIn.close()
--- teste.asm
+++ teste.asm
@@ -1,16 +1,26 @@
- .text
- .globl  main
+.text
+.globl  main
 
+ teste2:
+ 	slt $t1, $t2, $t3
+ 
  main:
- 	beq $t1, $t3, loop
 	sub $t1, $s3, $s4
 	or $t0, $t1, $t2
+	beq $t1, $t5, loop
 	sll $t1, $t2, 4
 	slt $t1, $t2, $t3
+	bne $t5, $t8, teste
 	addu $t1, $t2, $s3
 	subu $t7, $t6, $t4
+	beq $t7, $t6, teste2
 	srl $t0, $t1, 4
 	srl $t0, $t1, 4
-loop:
+
+teste:
 	sub $t1, $s3, $s4
 	or $t0, $t1, $t2
+
+loop:
+	slt $t1, $t2, $t3
+	addu $t1, $t2, $s3
